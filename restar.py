a4ac8879aa3e7a7aec046f8e46de695af684bb42
momoladebrouill <momoladebrouill@gmail.com>
2021-06-21 11:05:51 +0200

    ajout du mode fps en appuyant sur a

--- restar.py
+++ restar.py
@@ -34,9 +34,7 @@ while B:
         indx=0
     
     rays=[]
-    if fpsmode:
-        pg.mouse.set_pos((min(wind)+,wind[1]/2))
-    else:
+    if not fpsmode:
         decal=pg.mouse.get_pos()
         decal=math.atan2(decal[1]/coef-pos[1],decal[0]/coef-pos[0])-ouverture/2
     
@@ -50,6 +48,7 @@ while B:
         rpos[0]-=test[0]
         rpos[1]-=test[1]
         rpos.append(math.dist(rpos,pos))
+        rpos.append(ray)
         rays.append(rpos)
     ind=0
     for ray in rays:
@@ -79,20 +78,46 @@ while B:
             B=0
         
         elif event.type==pg.KEYDOWN:
-            if event.key==pg.K_z:moving[1]=-1
-            elif event.key==pg.K_q:moving[0]=-1
-            elif event.key==pg.K_s:moving[1]=1
-            elif event.key==pg.K_d:moving[0]=1
+            if fpsmode:
+                if event.key== pg.K_z:
+                    moving=[math.cos(decal),math.sin(decal)]
+                if event.key== pg.K_q:
+                    moving=[math.cos(decal-math.pi/2),math.sin(decal-math.pi/2)]
+                if event.key== pg.K_s:
+                    moving=[-math.cos(decal),-math.sin(decal)]
+                if event.key== pg.K_d:
+                    moving=[math.cos(decal+math.pi/2),math.sin(decal+math.pi/2)]
+            else:
+                if event.key==pg.K_z:moving[1]=-1
+                elif event.key==pg.K_q:moving[0]=-1
+                elif event.key==pg.K_s:moving[1]=1
+                elif event.key==pg.K_d:moving[0]=1
 
         elif event.type==pg.KEYUP:
+            if fpsmode:moving=[0,0]
             if event.key==pg.K_z:moving[1]=0
             elif event.key==pg.K_q:moving[0]=0
             elif event.key==pg.K_s:moving[1]=0
             elif event.key==pg.K_d:moving[0]=0
-            elif event.key==pg.K_a:fpsmode=not fpsmode
-
+            elif event.key==pg.K_a:
+                fpsmode=not fpsmode
+                if fpsmode:
+                    pg.mouse.set_pos((min(wind)+(max(wind)-min(wind))/2,wind[1]/2))
+                pg.mouse.set_visible( not fpsmode)
+        elif event.type==pg.MOUSEMOTION and fpsmode:
+            rel=list(pg.mouse.get_pos())
+            rel[0]=rel[0]-min(wind)-(max(wind)-min(wind))/2
+            rel[1]=rel[1]-wind[1]/2
+            decal+=rel[0]/wind[1]
+            pg.mouse.set_pos((min(wind)+(max(wind)-min(wind))/2,wind[1]/2))
         elif event.type==pg.MOUSEBUTTONUP:
-            if not fpsmode:
+            if fpsmode:
+                rayon=rays[len(rays)//2]
+                if event.button==1:
+                    carte[int(rayon[1])][int(rayon[0])]=1
+                else:
+                    carte[int(rayon[1]+math.sin(rayon[3]))][int(rayon[0]+math.cos(rayon[3]))]=0
+            else:
                 npos=pg.mouse.get_pos()
                 carte[int(npos[1]/coef)][int(npos[0]/coef)]=int(event.button==1)
         elif event.type==pg.VIDEORESIZE:
